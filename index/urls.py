0f7903ff992f7265fda9861d4979fabad20d3e78
Tony Ritrovato <antonioritrovato17@gmail.com>
2022-11-08 17:43:54 +0100

    add changes

--- index/urls.py
+++ index/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('form', views.form),
-    path('', views.index)
+    path('form', views.form, name='form'),
+    path('', views.index, name='index')
 ]
