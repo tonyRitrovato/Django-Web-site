c9a93640d2e881831d754550f3741e4de808a3aa
Tony Ritrovato <antonioritrovato17@gmail.com>
2022-11-10 11:01:31 +0100

    databases initialization

--- index/admin.py
+++ index/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Registrazione
 # Register your models here.
+
+admin.site.register(Registrazione)
--- index/models.py
+++ index/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Registrazione(models.Model):
+    nome = models.CharField(max_length=200)
+    cognome = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    numeroBiglietti = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.email
+
